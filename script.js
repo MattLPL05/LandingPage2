c1967e8ff9357edf4b5518fdb4875aacd19b33da
MattLPL <MattLPL05@gmail.com>
2026-04-15 15:42:06 +0200

    upgraded accordeon and optimised images

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //https://www.w3schools.com/howto/howto_js_accordion.asp
+
+function primaryJSLoader()
+{
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active-acc");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+        panel.style.display = "none";
+        } else {
+        panel.style.display = "block";
+        }
+    });
+    }
+}
+
+
+console.log("e");
+document.addEventListener("DOMContentLoaded", primaryJSLoader);
