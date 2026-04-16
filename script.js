83606e7acc626775f8800d99a4df77a589179169
MattLPL <MattLPL05@gmail.com>
2026-04-16 13:30:27 +0200

    Finished accordeon tab and started gallery tab

--- script.js
+++ script.js
@@ -5,21 +5,38 @@ function primaryJSLoader()
     var acc = document.getElementsByClassName("accordion");
     var i;
 
-    for (i = 0; i < acc.length; i++) {
+   /* for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
+        // Toggle between adding and removing the "active" class,
+       // to highlight the button that controls the panel 
         this.classList.toggle("active-acc");
 
-        /* Toggle between hiding and showing the active panel */
+        // Toggle between hiding and showing the active panel 
         var panel = this.nextElementSibling;
         if (panel.style.display === "block") {
-        panel.style.display = "none";
+        panel.style.display = "none"; 
         } else {
         panel.style.display = "block";
         }
     });
-    }
+    } */
+
+    // code above works with a Display: none, and has no animation
+    //code below works with maxHeight, and is fully animated that way
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+          this.classList.toggle("active-acc");
+          var panel = this.nextElementSibling;
+          if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+          }
+        });
+      }
+
+    
 }
 
 
