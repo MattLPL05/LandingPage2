e3ad45db3aa35ae41c78993f3def3f9665c99257
MattLPL <MattLPL05@gmail.com>
2026-04-17 12:17:43 +0200

    added top scrollbar and made navbar sticky

--- script.js
+++ script.js
@@ -36,8 +36,17 @@ function primaryJSLoader()
         });
       }
 
-    
+
+      window.onscroll = function() {myFunction()};
+
+      function myFunction() {
+        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var scrolled = (winScroll / height) * 100;
+        document.getElementById("le-bar").style.width = scrolled + "%";
+      }
 }
+
 
 
 document.addEventListener("DOMContentLoaded", primaryJSLoader);
