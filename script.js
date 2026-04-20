93641f72e9857a4e52a7c1e0365466fbf162a28a
MattLPL <MattLPL05@gmail.com>
2026-04-20 15:39:52 +0200

    finished the gallery code (close up + category switching)

--- script.js
+++ script.js
@@ -54,13 +54,116 @@ function primaryJSLoader()
     var Pics = document.getElementsByClassName("gallery-image-container-inner");
     var Piclogs = Array.from(Pics)
 
-    console.log(Piclogs)
+
+    var gallerycontainer = document.getElementsByClassName("gallery-image-fullscreen-container");
+    var gallerycontainerArray = Array.from(gallerycontainer);
+
     Piclogs.forEach(element => {
-      console.log(element);
+      //console.log(element);
       element.addEventListener(`click`, event => {
-        console.log("e");
+        //console.log("e");
+        var collectedPicture = element.getElementsByClassName("gallery-image");
+        var picture = Array.from(collectedPicture);
+        //Gets the fullscreen image path
+        var imgcontainer = document.querySelector(".gallery-image-fullscreen-picture")
+        var imgsourse = imgcontainer.querySelector("img").getAttribute("src");
+        var picturesourse = picture[0].getAttribute("src");
+
+        //makes the thing visible
+        gallerycontainerArray[0].style.display = "block";
+        //replaces the old picture with the new picture
+        //console.log(imgsourse);
+        //imgsourse = picturesourse;
+        imgcontainer.querySelector("img").setAttribute("src", picturesourse);
+        //console.log(imgsourse);
+        
+        
+
       });
     });
+
+    //for closing the gallery fullscreen picture
+
+    var Picsbac = document.getElementsByClassName("gallery-image-fullscreen-background");
+    var Piclogs2 = Array.from(Picsbac)
+
+    Piclogs2.forEach(element => {
+      //console.log(element);
+      element.addEventListener(`click`, event => {
+        //console.log("e2");
+        gallerycontainerArray[0].style.display = "none";
+      });
+    });
+
+
+    //for handling gallery categories
+    var galleryCategory = document.getElementsByClassName("gallery-category");
+    var galleryCategoryArrr = Array.from(galleryCategory)
+    var starshipCat =  document.getElementsByClassName("starships-category");
+    var starshipcatArr = Array.from(starshipCat)
+    var sightCat = document.getElementsByClassName("sights-category");
+    var sightCatArr = Array.from(sightCat)
+
+    galleryCategoryArrr.forEach(element => {
+      //console.log(element);
+      
+      element.addEventListener(`click`, event => {  
+        
+        RemoveCategoryEffect();
+        event.target.classList.toggle('gallery-category-active');
+       
+        AjustCategoryImages();
+        //element.classList.remove("gallery-category-active");
+
+      });
+    });
+
+    function RemoveCategoryEffect() //rerun the disabling on effects on all buttons
+    {
+      galleryCategoryArrr.forEach(element => {
+        if(element.classList.contains("gallery-category-active"))
+        {
+          element.classList.remove("gallery-category-active")
+        }
+        
+      });
+    }
+
+    function AjustCategoryImages() //rerun the code to make sure images appear properly
+    {
+      galleryCategoryArrr.forEach(element => {
+        if(element.classList.contains("gallery-category-all") && element.classList.contains("gallery-category-active"))
+        {
+          starshipcatArr.forEach(elementInner => {
+            elementInner.style.display = "block";
+          });
+          sightCatArr.forEach(elementInner => {
+            elementInner.style.display = "block";
+          });
+        }
+
+        if(element.classList.contains("gallery-category-ships") && element.classList.contains("gallery-category-active"))
+        {
+          starshipcatArr.forEach(elementInner => {
+            elementInner.style.display = "block";
+          });
+          sightCatArr.forEach(elementInner => {
+            elementInner.style.display = "none";
+          });
+        }
+
+        if(element.classList.contains("gallery-category-sights") && element.classList.contains("gallery-category-active"))
+        {
+          starshipcatArr.forEach(elementInner => {
+            elementInner.style.display = "none";
+          });
+          sightCatArr.forEach(elementInner => {
+            elementInner.style.display = "block";
+          });
+        }
+        
+      });
+    }
 }
 
 
